3cc35a9f6a4b4fffbb379a96b165c8adc45585d8
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2021-12-24 15:07:30 +0800

    linebot

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
-
+from linebot import (
+    LineBotApi, WebhookHandler
+)
 app=Flask(__name__)
 
 # app.static_folder='static'
@@ -8,6 +10,13 @@ app=Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+
+@app.route('/messages')
+def home():
+    return 'fuck'
+
 
 if __name__=='__main__':
     app.run()
