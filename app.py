5859e2084c5175197fa6d4d9e83827789c6098f8
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2021-12-24 15:13:03 +0800

    push message

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import render_template
 from linebot import (
     LineBotApi, WebhookHandler
 )
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 app=Flask(__name__)
 
 # app.static_folder='static'
@@ -11,11 +14,16 @@ app=Flask(__name__)
 def home():
     return render_template('home.html')
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+line_bot_api = LineBotApi('Qi7XZPL25/HKPXECrtPOJjA3mwTPAkS+eppoWlXNDhAEVe4qmyuTX7tsdjgBA9z27xzOyPo+eqP5oJMCf6+Gz1S5JzisSb2A9Viap0sWrajVfWAeld1ZefrGjWOWFOtcPZLLmsb4SYPmHV+pdrP8ygdB04t89/1O/w1cDnyilFU=')
 
 @app.route('/messages')
 def home():
     return 'fuck'
+@app.route('/push')
+def push():
+    line_bot_api.push_message(2632205727,TextSendMessage(text='fuck'))
+    return "1"
+
 
 
 if __name__=='__main__':
