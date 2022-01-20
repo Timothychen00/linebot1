01d89f2d792ce8527e3967ad2c63e70e8873a409
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-01-20 22:43:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from linebot.models import TextSendMessage,MessageEvent,TextMessage
 from linebot.exceptions import LineBotApiError,InvalidSignatureError
 import pymongo,os
 from dotenv import load_dotenv
+from flask_debug import Debug
 load_dotenv()
 
 
 client = pymongo.MongoClient("mongodb+srv://"+os.environ['DB_USER']+":"+os.environ['DB_PASSWORD']+"@cluster0.mgwi6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 app=Flask(__name__)
-
+Debug(app)
 
 
 # db = client.Flask
@@ -22,7 +23,7 @@ try:
     line_bot_api = LineBotApi('Qi7XZPL25/HKPXECrtPOJjA3mwTPAkS+eppoWlXNDhAEVe4qmyuTX7tsdjgBA9z27xzOyPo+eqP5oJMCf6+Gz1S5JzisSb2A9Viap0sWrajVfWAeld1ZefrGjWOWFOtcPZLLmsb4SYPmHV+pdrP8ygdB04t89/1O/w1cDnyilFU=')
     handler = WebhookHandler('3d35c2610fb78762643b8b7a58414ba7')
 except:
-    print
+    print(1)
 @app.route('/')
 def home():
     return render_template('home.html')
