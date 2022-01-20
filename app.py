0c0f2b4d2f38df4f8d3796da6101c944a9a6e5f7
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-01-20 23:46:52 +0800

    123

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ client = pymongo.MongoClient("mongodb+srv://"+os.environ['DB_USER']+":"+os.envir
 app=Flask(__name__)
 
 
-# db = client.Flask
-# collection=db.settings
-# result=collection.find_one()
+db = client.Flask
+collection=db.settings
+result=collection.find_one()
 
 # print(result)
 
-# line_bot_api = LineBotApi('Qi7XZPL25/HKPXECrtPOJjA3mwTPAkS+eppoWlXNDhAEVe4qmyuTX7tsdjgBA9z27xzOyPo+eqP5oJMCf6+Gz1S5JzisSb2A9Viap0sWrajVfWAeld1ZefrGjWOWFOtcPZLLmsb4SYPmHV+pdrP8ygdB04t89/1O/w1cDnyilFU=')
-# handler = WebhookHandler('3d35c2610fb78762643b8b7a58414ba7')
+line_bot_api = LineBotApi('Qi7XZPL25/HKPXECrtPOJjA3mwTPAkS+eppoWlXNDhAEVe4qmyuTX7tsdjgBA9z27xzOyPo+eqP5oJMCf6+Gz1S5JzisSb2A9Viap0sWrajVfWAeld1ZefrGjWOWFOtcPZLLmsb4SYPmHV+pdrP8ygdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('3d35c2610fb78762643b8b7a58414ba7')
 
 @app.route('/')
 def home():
@@ -29,23 +29,23 @@ def home():
 # def messages():
 #     return 'fuck'
 
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     # get X-Line-Signature header value
-#     signature = request.headers['X-Line-Signature']
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-#     # handle webhook body
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         print("Invalid signature. Please check your channel access token/channel secret.")
-#         abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
 
-#     return 'OK'
+    return 'OK'
 
 # def load_settings():
 #     collection=db.settings
