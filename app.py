d309a5cc524fddd7dafd4170017a0aa002a45ead
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-01-20 16:06:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask,request,abort,render_template
-from linebot import LineBotApi,WebhookHandler
-from linebot.models import TextSendMessage,MessageEvent,TextMessage
-from linebot.exceptions import LineBotApiError,InvalidSignatureError
-import pymongo,os
-from dotenv import load_dotenv
-load_dotenv()
+# from linebot import LineBotApi,WebhookHandler
+# from linebot.models import TextSendMessage,MessageEvent,TextMessage
+# from linebot.exceptions import LineBotApiError,InvalidSignatureError
+# import pymongo,os
+# from dotenv import load_dotenv
+# load_dotenv()
 
 
-client = pymongo.MongoClient("mongodb+srv://"+os.environ['DB_USER']+":"+os.environ['DB_PASSWORD']+"@cluster0.mgwi6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+# client = pymongo.MongoClient("mongodb+srv://"+os.environ['DB_USER']+":"+os.environ['DB_PASSWORD']+"@cluster0.mgwi6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 app=Flask(__name__)
 
@@ -18,33 +18,33 @@ db = client.Flask
 # result=collection.find_one()
 
 # print(result)
-line_bot_api = LineBotApi('Qi7XZPL25/HKPXECrtPOJjA3mwTPAkS+eppoWlXNDhAEVe4qmyuTX7tsdjgBA9z27xzOyPo+eqP5oJMCf6+Gz1S5JzisSb2A9Viap0sWrajVfWAeld1ZefrGjWOWFOtcPZLLmsb4SYPmHV+pdrP8ygdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('3d35c2610fb78762643b8b7a58414ba7')
+# line_bot_api = LineBotApi('Qi7XZPL25/HKPXECrtPOJjA3mwTPAkS+eppoWlXNDhAEVe4qmyuTX7tsdjgBA9z27xzOyPo+eqP5oJMCf6+Gz1S5JzisSb2A9Viap0sWrajVfWAeld1ZefrGjWOWFOtcPZLLmsb4SYPmHV+pdrP8ygdB04t89/1O/w1cDnyilFU=')
+# handler = WebhookHandler('3d35c2610fb78762643b8b7a58414ba7')
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/messages')
-def messages():
-    return 'fuck'
+# @app.route('/messages')
+# def messages():
+#     return 'fuck'
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+# @app.route("/callback", methods=['POST'])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+#     # handle webhook body
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         print("Invalid signature. Please check your channel access token/channel secret.")
+#         abort(400)
 
-    return 'OK'
+#     return 'OK'
 
 # def load_settings():
 #     collection=db.settings
