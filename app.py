b5dc8e646b0c4b68760d7116f00f02fe05941874
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-01-20 08:15:03 +0800

    linbot webhook

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask
+from flask import Flask,request
 from flask import render_template
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-app=Flask(__name__)
-line_bot_api = LineBotApi('Qi7XZPL25/HKPXECrtPOJjA3mwTPAkS+eppoWlXNDhAEVe4qmyuTX7tsdjgBA9z27xzOyPo+eqP5oJMCf6+Gz1S5JzisSb2A9Viap0sWrajVfWAeld1ZefrGjWOWFOtcPZLLmsb4SYPmHV+pdrP8ygdB04t89/1O/w1cDnyilFU=')
-# app.static_folder='static'
+from linebot import LineBotApi,WebhookHandler
+from linebot.models import TextSendMessage,MessageEvent,TextMessage
+from linebot.exceptions import LineBotApiError,InvalidSignatureError
+import pymongo,os
+from dotenv import load_dotenv
+load_dotenv()
 
+
+client = pymongo.MongoClient("mongodb+srv://"+os.environ['DB_USER']+":"+os.environ['DB_PASSWORD']+"@cluster0.mgwi6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+app=Flask(__name__)
+
+
+
+db = client.Flask
+collection=db.settings
+result=collection.find_one()
+
+print(result)    
+# line_bot_api = LineBotApi('Qi7XZPL25/HKPXECrtPOJjA3mwTPAkS+eppoWlXNDhAEVe4qmyuTX7tsdjgBA9z27xzOyPo+eqP5oJMCf6+Gz1S5JzisSb2A9Viap0sWrajVfWAeld1ZefrGjWOWFOtcPZLLmsb4SYPmHV+pdrP8ygdB04t89/1O/w1cDnyilFU=')
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -19,12 +29,30 @@ def home():
 @app.route('/messages')
 def messages():
     return 'fuck'
-@app.route('/push')
-def push():
-    line_bot_api.push_message(2632205727,TextSendMessage(text='fuck'))
-    return "1"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
 
 
+def load_settings():
+    collection=db.settings
+    result=collection.find_one()
+    settings=result
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
