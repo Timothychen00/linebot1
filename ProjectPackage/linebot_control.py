6248389ce43e07c15762f3eaec92ddf09ad8b111
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-02-22 19:45:15 +0800

    lo

--- ProjectPackage/linebot_control.py
+++ ProjectPackage/linebot_control.py
@@ -34,7 +34,7 @@ def echo(event):
     elif re.match("!bot help",message):
         parameter.line_bot_api.reply_message(reply_token,TextSendMessage(text="Available Commands:\n"+str(parameter.bot['commands'])))
     elif re.match("!bot reload settings",message):
-        parameter.lßoad_settings()
+        parameter.load_settings()
         parameter.line_bot_api.reply_message(reply_token,TextSendMessage(text="reload settings"))
     elif re.match("!bot now bounded",message):
         parameter.line_bot_api.reply_message(reply_token,TextSendMessage(text=str(parameter.settings['user-id'])))
