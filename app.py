b8e5003f3459ab1a0ce7d3cea46e75cd17964e8c
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-02-22 20:29:06 +0800

    gunicorn

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,abort,render_template
+from flask import Flask
 import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask_apscheduler import APScheduler
@@ -28,8 +28,9 @@ def job3():
 if parameter.settings['notification-time']:
     scheduler.add_job(id='jobx', func=job3, trigger='cron', day='*',hour=parameter.settings['notification-time'].split(":")[0],minute=parameter.settings['notification-time'].split(":")[1])
 
-if __name__=='__main__':
-    scheduler.init_app(app)
-    scheduler.start()
+
+scheduler.init_app(app)
+scheduler.start()
+if __name__=='__main__':#不要把東西放在__name__這個if裡面，因為如果使用gunicorn啟動部署伺服器那__name__永遠不會是__main__
     app.run(use_reloader=False,debug=True,port=8080)
 
