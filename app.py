1366170befab16c55d93b9553724a1737ad101bd
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-05-03 12:10:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from Project.routes import app_route
 import os
 
 app=Flask(__name__,static_folder='Project/static/',template_folder='Project/temlpates/')
-app.secret_key=os.urandom(16).hex()
+app.secret_key=os.environ['secret']#os.urandom(16).hex()
 app.register_blueprint(app_route)
 
 if __name__=='__main__':#不要把東西放在__name__這個if裡面，因為如果使用gunicorn啟動部署伺服器那__name__永遠不會是__main__
