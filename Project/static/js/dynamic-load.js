aced1b24bc755da8762f8517bc9603c30ee0e89c
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2023-10-07 23:16:53 +0800

    初步修好

--- Project/static/js/dynamic-load.js
+++ Project/static/js/dynamic-load.js
@@ -1,101 +1,59 @@
 var page_index=1;
 var max_index=10;
-var type="";
-var t_month=null;
+var search_mode="total";
+//search
+//total
+//this_month
+//next_month
 
+//find_mode可以通過var也可以通過傳遞變數
+
+function change_mode(mode){
+    window.page_index=1;
+    document.getElementById('page_index').innerText=page_index;
+    window.search_mode=mode;
+    find(search_mode);
+}
 function this_month(){
     window.page_index=1;
     document.getElementById('page_index').innerText=page_index;
-    window.t_month="this_month";
-    window.type="dynamic";
-
-    value=(page_index-1)*25
-    if(value==0)
-        value=1;
-    find(type,"next-time","1",value);
+    window.search_mode="this_month";
+    // window.type="dynamic";
+    find(search_mode);
 }
 
 function next_month(){
     window.page_index=1;
     document.getElementById('page_index').innerText=page_index;
-    window.t_month="next_month";
-    window.type="search";
-
-    value=(page_index-1)*25
-    if(value==0)
-        value=1;
-    find(type,"next-time","1",value);
+    window.search_mode="next_month";
+    find(search_mode);
 }
 // alert(3);
 
-next_page=()=>{
+var next_page=()=>{
     window.page_index+=1;
-    // let now=document.getElementById('page_index').value;
-    let value=null;
-    if (t_month)
-        value="1";
-    // alert(t_month);
-    find("dynamic",t_month,value,page_index);
-    
+    find();//使用當前的var
     document.getElementById('page_index').innerText=page_index;
 }
 
-last_page=()=>{
+var last_page=()=>{
     if(page_index>1)
         window.page_index-=1;
-    
-    let value=null;
-    if (t_month)
-        value="1";
-        alert(t_month);
-    find("dynamic",t_month,value,page_index);
+    find();
     document.getElementById('page_index').innerText=page_index;
 
 }
 
-function getScrollTop()
-{
-    var dashboard=document.getElementById('dashboard');
-    var scrollTop = 0, bodyScrollTop = 0, documentScrollTop = 0;
-    if(dashboard){
-        bodyScrollTop = dashboard.scrollTop;
-    }
-    if(document.documentElement){
-        documentScrollTop = document.documentElement.scrollTop;
-    }
-scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
-return scrollTop;
-}
-
-function getScrollHeight(){
-        var dashboard=document.getElementById('dashboard');
-    　　var scrollHeight = 0, bodyScrollHeight = 0, documentScrollHeight = 0;
-    　　if(dashboard){
-    　　　　bSH = dashboard.scrollHeight;
-    　　}
-    　　if(document.documentElement){
-    　　　　dSH = document.documentElement.scrollHeight;
-    　　}
-    scrollHeight = (bSH - dSH > 0) ? bSH : dSH ;
-    　　return scrollHeight;
-    }
-
-function getWindowHeight(){
-    var dashboard=document.getElementById('dashboard');
-    var windowHeight = 0;
-    if(document.compatMode == "CSS1Compat"){
-        windowHeight = document.documentElement.clientHeight;
-    }else{
-        windowHeight = dashboard.clientHeight;
-    }
-    return windowHeight;
-}
-
-var month='';
-function find(type='dynamic',key_v=null,value_v=null,start=1){
+function find(type='dynamic',key_v=null,value_v=null){
 //    alert(2);
-    if (t_month)
-        month=t_month
+    var  start=(page_index-1)*25;;
+    if(search_mode=='total')
+    {
+    }else if(search_mode=='this_month' || search_mode=='next_month'){
+        key_v='next-time';
+        value_v='1';
+    }
+
     if (key_v==null)
     {
         key_v=document.getElementById('type').value;
@@ -103,22 +61,11 @@ function find(type='dynamic',key_v=null,value_v=null,start=1){
     }
     console.log(value);
     xhr=new XMLHttpRequest();
-    if(page_index!=1)
-    {
-        
-        start=(page_index-1)*25;
-        if(start==0)
-            start=1;
-        
-    }
+    
     // alert(page_index);
     // alert(start);
     // var start=1;
     table=document.querySelector("#place");
-    if (table){
-        console.log(table.childElementCount);//因爲每一個tr的編號不一定是間隔1，所以要接續之前結果要用childElementCount
-        // start=table.childElementCount;
-    }
     xhr.onload = function(e) {
         // alert(1);
         var place=document.getElementById('place');
@@ -165,40 +112,22 @@ function find(type='dynamic',key_v=null,value_v=null,start=1){
     // alert(month);
     var place=document.getElementById('place');
     url='';
+
+    
     if (type=='dynamic')
     {
-        if (month==null || month=='none')
+        if (search_mode=='total')
             url='/customers/?key='+key_v+'&value='+value_v+"&type=json&start="+start+"&length=25";
         else
-            url='/customers/?key='+key_v+'&value='+value_v+"&type=json&start="+start+"&length=25&month="+month;
+            url='/customers/?key='+key_v+'&value='+value_v+"&type=json&start="+start+"&length=25&month="+search_mode;
     }else{
         place.innerHTML='';
-        if (month==null || month=='none')
+        if (search_mode=='total')
         {
             url='/customers/?key='+key_v+'&value='+value_v+"&type=json";
         }else
-            url='/customers/?key='+key_v+'&value='+value_v+"&type=json&month="+month;
+            url='/customers/?key='+key_v+'&value='+value_v+"&type=json&month="+search_mode;
     }
     xhr.open('GET',url,false)
     xhr.send()
 }
-// find("dynamic");
-
-
-fetch('/api/results_count', { method: "GET",mode: 'no-cors' })
-    .then((res)=>(res.text()))
-    .then((res)=>{
-        // page_index=Number(res);
-        let counts=Number(res);
-
-        let pages=Math.ceil(counts/25);
-        max_index=pages;
-        // alert(pages);
-        // console.log(res);
-        // if ()
-    })
-
-// refresh_bar=()=>{
-//     if(page_index==pages)
-
-// }
